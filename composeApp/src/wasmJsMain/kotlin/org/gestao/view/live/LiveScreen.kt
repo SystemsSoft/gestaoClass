3a0f7aa2c3440ea8acf4eb559001ab89a6f09f98
bruno.belloti <bruno.belloti@ecocentauro.com.br>
2025-05-25 13:30:15 -0300

    feat arquitetura

--- composeApp/src/wasmJsMain/kotlin/org/gestao/view/live/LiveScreen.kt
+++ composeApp/src/wasmJsMain/kotlin/org/gestao/view/live/LiveScreen.kt
@@ -1,39 +1,55 @@
 package org.gestao.view.live
 
-
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import kotlinx.browser.document
-import org.w3c.dom.HTMLIFrameElement
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import gestaoweb.bbf.com.util.Theme.backgroundCard
+import kotlinx.browser.window
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.gestao.view.classSelector
+import org.gestao.viewmodel.bindAccessRegistration
 
-
+val isMeeting = MutableStateFlow(false)
 @Composable
 fun liveClassScreen() {
-    val iframeId = "videoCallFrame"
-    val containerId = "iframe-container"
-    var iframeAppended by remember { mutableStateOf(false) }
+    startMeet()
+    Card(
+        modifier = Modifier
+            .width(1200.dp)
+            .padding(start = 150.dp,top = 100.dp,end = 100.dp, bottom = 50.dp)
+    ) {
+        Column(
+            modifier = Modifier.background(backgroundCard)
+        ) {
+            classSelector()
 
-    LaunchedEffect(Unit) {
-        if (!iframeAppended) {
-            val container = document.getElementById(containerId)
-            if (container != null && container.childElementCount == 0) {
-                val iframe = document.createElement("iframe") as HTMLIFrameElement
-                iframe.id = iframeId
-                iframe.src = "meet.html"
-                iframe.width = "800"
-                iframe.height = "600"
-                iframe.style.border = "none"
-                iframe.style.display = "block"
-                iframe.style.margin = "0 auto"
-                container.appendChild(iframe)
-                iframeAppended = true
+            Button(
+                onClick = { isMeeting.value = true },
+                modifier = Modifier
+                    .padding(40.dp)
+                    .align(Alignment.CenterHorizontally),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Text(text = "Começar Aula", color = Color.Black)
             }
         }
     }
+}
 
-    DisposableEffect(Unit) {
-        onDispose {
-            document.getElementById(iframeId)?.remove()
-            iframeAppended = false
-        }
+@Composable
+fun startMeet() {
+    if (isMeeting.collectAsState().value) {
+        window.open("", "_blank")
     }
 }
+
