ae73183750d5fe02a875653fba3e8ada5156df2b
bruno.belloti <bruno.belloti@ecocentauro.com.br>
2025-07-02 16:35:47 -0300

    feat url

--- composeApp/src/wasmJsMain/kotlin/org/gestao/util/BaseApi.kt
+++ composeApp/src/wasmJsMain/kotlin/org/gestao/util/BaseApi.kt
@@ -3,5 +3,4 @@ package org.gestao.util
 object BaseApi {
     const val BASE_S3 = "s3.us-east-2.amazonaws.com"
     const val BASE_SERVER = "https://athennaserver-5c57d33a2f13.herokuapp.com/"
-//"https://bbfserver.xyz:443/"
 }
